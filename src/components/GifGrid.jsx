9a28374d139ea61edf859927c4d3c1163cfa3223
Rubén Seco <128716522+RubenSeco@users.noreply.github.com>
2023-11-09 22:53:43 -0300

    Actualización 9/11/23

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from 'react';
-import { GifItem } from './GifItem';
+import { GifItem } from '../components';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
---
+++ tests/components/GifItem.test.jsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react';
+import { GifItem } from '../../src/components/GifItem';
+
+describe('Evaluar GifItem', () => {
+  const title = 'Saitama';
+  const url = 'https://one-punch.com/saitama.jpg';
+  test('Tiene que hacer match con el snapshot', () => {
+    const { container } = render(
+      <GifItem
+        title={title}
+        url={url}
+      />
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test('Debe mostrar la imagen con el URL y el ALT indicado', () => {
+    render(
+      <GifItem
+        title={title}
+        url={url}
+      />
+    );
+    // screen.debug();
+    // expect(screen.getByRole('img').src).toBe(url);
+    const { src, alt } = screen.getByRole('img');
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+  });
+
+  test('Debe mostrar el título en el componente', () => {
+    render(
+      <GifItem
+        title={title}
+        url={url}
+      />
+    );
+
+    expect(screen.getByText(title)).toBeTruthy();
+  });
+});
